c0528f79f5438572b551b812daf1bd40727acd12
muqeema <muqeembca786@gmail.com>
2025-04-24 15:44:49 +0530

    added CircleCIDemoTests

---
+++ CircleCIDemoTests/CircleCIDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  CircleCIDemoTests.swift
+//  CircleCIDemoTests
+//
+//  Created by Muqeem Ahmad on 24/04/25.
+//
+
+import XCTest
+@testable import CircleCIDemo
+
+final class CircleCIDemoTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual(2 + 2, 4)
+    }
+}
